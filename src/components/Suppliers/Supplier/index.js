dff69c956e0b170b0cf9adeb76e8dcb762c8db1f
rubensato <rubensato@gmail.com>
2021-11-23 22:17:24 -0300

    Supplier list item component created with table cell structure

--- src/components/Suppliers/Supplier/index.js
+++ src/components/Suppliers/Supplier/index.js
@@ -1,9 +1,31 @@
+import React from 'react';
+
 const Supplier = () => {
-    return (
-      <div>
-          Supplier
-      </div>)
-      ;
-  };
-  
-  export default Supplier;
+  return (
+    <tr>
+      <td>
+        <span className='font-weight-bold'><p>Nombre x</p></span>
+      </td>
+      <td><p>Email x</p></td>
+      <td>Telefono x</td>
+      <td className='actions'>
+        <button
+          type='button'
+          onClick=''
+          className='btn btn-primary m-1'
+        >
+          Editar
+        </button>
+        <button
+          type='button'
+          className='btn btn-danger m-1'
+          onClick=''
+        >
+          Eliminar
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default Supplier;
