8738f28ce29c5e1b4830b6d99a4f6eeba04f8a4a
rubensato <rubensato@gmail.com>
2021-11-24 19:18:30 -0300

    some corrections after merge new PR from development branch

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,10 +2,6 @@ import { combineReducers } from "redux";
 import clientsReducer from "./clientsReducer";
 import productsReducer from "./productsReducer";
 import suppliersReducer from "./suppliersReducer";
-<<<<<<< HEAD
-
-=======
->>>>>>> development
 
 // Se utiliza un reducer Index que integrara todos los demas reducers.
 
